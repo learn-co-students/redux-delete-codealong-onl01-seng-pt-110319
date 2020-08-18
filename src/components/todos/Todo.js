3d99022c289a3a657dc21b73948d1e40697e304a
kierra <51520037+kierrakay@users.noreply.github.com>
2020-08-18 11:07:58 -0400

    Done.

--- src/components/todos/Todo.js
+++ src/components/todos/Todo.js
@@ -1,5 +1,16 @@
 import React from 'react'
 
-const Todo = props => <li>{props.text}</li>
-
+const Todo = props => {
+    return (
+        <div>
+            <span>{props.todo.text}</span>
+            <button onClick={() => props.delete(props.todo.id)}>DELETE</button>
+            {/* <span>{props.text} </span> */}
+            {/* <button onClick={() => props.delete(props.text)}>DELETE</button> */}
+        </div>
+    )
+}
 export default Todo;
+
+// We're providing a definition for an anonymous function. Inside the definition, we're calling props.delete, and passing in the only other prop available, props.text
+// Back in our connected TodosContainer, when this delete button is clicked, the value of props.text is passed into our dispatched action as the payload
--- src/reducers/manageTodo.js
+++ src/reducers/manageTodo.js
@@ -1,11 +1,28 @@
+import uuid from 'uuid'
+//above gives every todo a unique id. without it it will
+//delte every text that matches. so if i have two hey todos on the dom and delete one 
+//it would delete both without this. cause we have playload in todoscontainer delete todoText
+
 export default function manageTodo(state = {
   todos: [],
 }, action) {
-  console.log(action)
+  // console.log(action) 
+  //if uncommented it wont delete
   switch (action.type) {
     case 'ADD_TODO':
 
-      return { todos: state.todos.concat(action.payload.text) };
+    const todo = {
+      id: uuid(),
+      text: action.payload.text
+    }
+
+    return { todos: state.todos.concat(todo) };
+      // return { todos: state.todos.concat(action.payload.text) };
+      // Using uuid() will generate a long random string each time a todo is created. Now, instead of just storing an array of strings in our store, we'll be storing an array of objects.
+      // This causes a problem 'downstream', though: we need to update our TodosContainer to pass the correct content.Using uuid() will generate a long random string each time a todo is created. Now, instead of just storing an array of strings in our store, we'll be storing an array of objects.
+      // This causes a problem 'downstream', though: we need to update our TodosContainer to pass the correct content.
+    case 'DELETE_TODO':
+      return {todos: state.todos.filter(todo => todo.id !== action.payload)}
 
     default:
       return state;
